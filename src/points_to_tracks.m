2173f7dbdb12802db652989db8b72293e8467415
Jack Valmadre <jack.valmadre@gmail.com>
2013-01-23 11:15:03 +1000

    Removed left over parameter

--- src/points_to_tracks.m
+++ src/points_to_tracks.m
@@ -1,12 +1,11 @@
 % Parameters:
 % points -- num_frames x num_points x num_dims (usually 2 or 3)
 
-function tracks = points_to_tracks(points, mask);
+function tracks = points_to_tracks(points)
   num_frames = size(points, 1);
   num_points = size(points, 2);
   num_dims = size(points, 3);
 
-  % Convert from points and mask to tracks.
   tracks = struct('frames', {}, 'points', {});
   for i = 1:num_points
     point_subset = reshape(points(:, i, :), [num_frames, num_dims]);
