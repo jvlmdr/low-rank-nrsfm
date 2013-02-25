fa0ee6b86a633632b542b53b6e3c63821c985e41
Jack Valmadre <jack.valmadre@gmail.com>
2013-02-26 09:43:39 +1000

    Added noise to noise experiment

--- src/noise_experiment.m
+++ src/noise_experiment.m
@@ -4,6 +4,9 @@ downsample = 8;
 
 omega_stddev = 5 * pi / 180;
 scale_stddev = sqrt(2);
+noise_stddevs = [0, 0.01, 1, 100];
+
+num_noises = length(noise_stddevs);
 
 % Load some mocap sequences.
 if ~exist('../data/mocap-data.mat', 'file')
@@ -25,16 +28,21 @@ K = 4;
 % Project on to low rank.
 for i = 1:num_sequences
   S = sequences(:, :, :, i);
-  % Subtract 3D mean from each frame.
+
+  % Subtract 3D centroid from each frame.
   mu = mean(S, 2);
   S = bsxfun(@minus, S, mu);
+
   % [F, P, 3] -> [F, 3P]
   S = reshape(S, [num_frames, 3 * num_points]);
-  [U, D, V] = svd(S);
-  S = U(:, 1:K) * D(1:K, 1:K) * V(:, 1:K)';
-  % [F, P, 3] -> [F, 3P]
+  % Project on to set of low rank matrices.
+  S = project_rank(S, K);
+  % [F, 3P] -> [F, P, 3]
   S = reshape(S, [num_frames, num_points, 3]);
-  %S = bsxfun(@plus, S, mu);
+
+  % Restore centroid.
+  S = bsxfun(@plus, S, mu);
+
   sequences(:, :, :, i) = S;
 end
 
@@ -54,17 +62,45 @@ solvers = [...
 num_solvers = numel(solvers);
 
 % Solve reconstruction using each method.
-residuals = zeros(num_sequences, num_solvers);
+residuals = zeros(num_sequences, num_noises, num_solvers);
 
 for i = 1:num_sequences
-  fprintf('Sequence %d\n', i);
-
   scene = scenes(i);
 
-  for j = 1:num_solvers
-    solver = solvers(j);
+  for j = 1:num_noises
+    noise_stddev = noise_stddevs(j);
+    fprintf('Sequence %d\n', i);
 
-    solution = solver.solve(scene.projections, K);
-    residuals(i, j) = min_shape_error(scene.points, solution);
+    for k = 1:num_solvers
+      solver = solvers(k);
+
+      noise = noise_stddev * randn(num_frames, num_points, 2);
+      solution = solver.solve(scene.projections + noise, K);
+
+      residuals(i, j, k) = min_shape_error(scene.points, solution);
+      residuals(i, j, k)
+    end
   end
 end
+
+residuals = permute(residuals, [1, 3, 2]);
+
+for i = 1:num_noises
+  noise_stddev = noise_stddevs(i);
+  figure;
+  boxplot(residuals(:, :, i));
+  hold on;
+  plotSpread(residuals(:, :, i));
+  hold off;
+  grid on;
+  axis([0, num_solvers + 1, 0, max(residuals(:)) * 1.05]);
+  title(sprintf('Noise level %g', noise_stddev));
+  set(gca, 'XTick', 1:num_solvers);
+  set(gca, 'XTickLabel', {solvers.name});
+  filename = sprintf('../figures/noise/%d-%g.eps', i, noise_stddev);
+  print(filename, '-depsc2');
+  unix(['epstopdf ', filename]);
+  unix(['rm ', filename]);
+end
+
+residuals = permute(residuals, [1, 3, 2]);
