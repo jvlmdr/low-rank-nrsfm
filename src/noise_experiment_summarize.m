aee5e698ec9b4db47bef64297b177e75902d071c
Jack Valmadre <jack.valmadre@gmail.com>
2013-03-27 09:12:19 +1000

    Added planar benchmark reconstruction

--- src/noise_experiment_summarize.m
+++ src/noise_experiment_summarize.m
@@ -1,5 +1,4 @@
 shape_errors = zeros(num_frames, num_sequences, num_noises, num_solvers);
-
 for i = 1:num_sequences
   S = scenes(i).points;
   S = permute(S, [3, 2, 1]);
@@ -11,6 +10,21 @@ for i = 1:num_sequences
       for t = 1:num_frames
         shape_errors(t, i, j, k) = min_shape_error(S(:, :, t), X(:, :, t));
       end
+    end
+  end
+end
+
+benchmark_errors = zeros(num_frames, num_sequences, num_noises);
+for i = 1:num_sequences
+  S = scenes(i).points;
+  S = permute(S, [3, 2, 1]);
+
+  for j = 1:num_noises
+    projections = noisy_scenes(i, j).projections;
+    X = [projections; zeros(1, num_points, num_frames)];
+
+    for t = 1:num_frames
+      benchmark_errors(t, i, j) = min_shape_error(S(:, :, t), X(:, :, t));
     end
   end
 end
--- src/noise_experiment_visualize.m
+++ src/noise_experiment_visualize.m
@@ -1,21 +1,44 @@
-residuals = permute(residuals, [1, 3, 2]);
+num_sequences = length(scenes);
+num_noises = length(noise_stddevs);
+
+num_nrsfm_solvers = length(solvers.nrsfm_solvers);
+num_camera_solvers = length(solvers.camera_solvers);
+num_nrsfm_solvers_given_cameras = length(solvers.nrsfm_solvers_given_cameras);
+
+num_solvers = num_nrsfm_solvers + ...
+    num_camera_solvers * num_nrsfm_solvers_given_cameras;
+
+mean_errors = shiftdim(mean(shape_errors), 1);
+
+% [num_sequences, num_noises, num_solvers]
+%   -> [num_sequences, num_solvers, num_noises]
+mean_errors = permute(mean_errors, [1, 3, 2]);
+
+% Average over frames and sequences.
+mean_benchmark_errors = shiftdim(mean(mean(benchmark_errors)), 2)
 
 for i = 1:num_noises
   noise_stddev = noise_stddevs(i);
+  a = 0.5;
+  b = num_solvers + 0.5;
+
+  rel_errors = mean_errors(:, :, i) / mean_benchmark_errors(i);
+
   figure;
-  boxplot(residuals(:, :, i));
+  boxplot(rel_errors);
   hold on;
-  plotSpread(residuals(:, :, i));
+  plotSpread(rel_errors);
+  line([a, b], [1, 1]);
   hold off;
   grid on;
-  axis([0, num_solvers + 1, 0, max(residuals(:)) * 1.05]);
+
+  axis([a, b, 0, max(rel_errors(:)) * 1.05]);
   title(sprintf('Noise level %g', noise_stddev));
   set(gca, 'XTick', 1:num_solvers);
-  set(gca, 'XTickLabel', {solvers.name});
-  filename = sprintf('../figures/noise/%d-%g.eps', i, noise_stddev);
-  print(filename, '-depsc2');
-  unix(['epstopdf ', filename]);
-  unix(['rm ', filename]);
-end
+  %set(gca, 'XTickLabel', {solvers.name});
 
-residuals = permute(residuals, [1, 3, 2]);
+%  filename = sprintf('../figures/noise/%d-%g.eps', i, noise_stddev);
+%  print(filename, '-depsc2');
+%  unix(['epstopdf ', filename]);
+%  unix(['rm ', filename]);
+end
