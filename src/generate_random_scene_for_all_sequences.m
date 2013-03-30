c2a60eab29d456c1f055df6c91f7e5658d8792f1
Jack Valmadre <jack.valmadre@gmail.com>
2013-03-30 17:32:00 +1000

    Updated shape of structure data

--- src/generate_random_scene_for_all_sequences.m
+++ src/generate_random_scene_for_all_sequences.m
@@ -1,16 +1,16 @@
 % Generates weak-perspective cameras with projections for each sequence.
 %
 % Parameters:
-% sequences -- num_frames x num_points x 3 x num_sequences
+% sequences -- 3 x num_points x num_frames x num_sequences
 
 function scenes = generate_random_scene_for_all_sequences(sequences, ...
     omega_stddev, scale_stddev)
-  num_frames = size(sequences, 1);
   num_points = size(sequences, 2);
+  num_frames = size(sequences, 3);
   num_sequences = size(sequences, 4);
 
   % Convert to cell array of sequences.
-  sequences = mat2cell(sequences, num_frames, num_points, 3, ...
+  sequences = mat2cell(sequences, 3, num_points, num_frames, ...
       ones(num_sequences, 1));
   sequences = reshape(sequences, [num_sequences, 1]);
 
